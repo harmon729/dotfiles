562045f6e219e8bfffe625b5dbc31c5147a6d4f7
Harmon Hsu <xhx1719@gmail.com>
2025-11-27 18:19:11 +0800

    Add ~/yank.buf to store copied content from nvim and tmux

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,15 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+local buffer_sync = require("custom.YankPost")
+
+vim.api.nvim_create_augroup("YankBufferPost", { clear = true })
+
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+  group = "YankBufferPost",
+  callback = function(_)
+    -- 延时执行：确保 Tmux 缓冲区已更新
+    vim.defer_fn(buffer_sync.sync_to_file, 50)
+  end,
+})
