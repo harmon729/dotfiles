ef672590ab2409abf57bfa84d45b0bb9ec3efd7c
Harmon Hsu <xhx1719@gmail.com>
2026-02-09 15:33:26 +0800

    fix(wsl): 🛠️ Resolve merge conflicts in wsl

--- bash/.config/bash/path.bash
+++ bash/.config/bash/path.bash
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
 if [ -d "$HOME/.local/bin" ]; then
-    PATH="$HOME/.local/bin:$PATH"
+  PATH="$HOME/.local/bin:$PATH"
 fi
 
 if [ -d "$HOME/bin" ]; then
-    PATH="$HOME/bin:$PATH"
+  PATH="$HOME/bin:$PATH"
+fi
+
+if [ -d "/usr/lib/wsl/lib" ]; then
+  PATH="/usr/lib/wsl/lib:$PATH"
 fi
 
 export PATH
