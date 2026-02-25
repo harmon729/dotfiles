cefa350c55660ecf20ab69ac02891ab52ee76366
Harmon Hsu <xhx1719@gmail.com>
2026-02-25 14:39:58 +0800

    feat(zsh): ✨ add global aliases for redirection and hash for dotfiles

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -13,3 +13,15 @@ alias lg='lazygit'
 
 alias shc='$EDITOR ${HOME}/.zshrc'
 alias shr='omz reload'
+
+# Redirect stderr to /dev/null
+alias -g NE='2>/dev/null'
+
+# Redirect stdout to /dev/null
+alias -g NO='>/dev/null'
+
+# Redirect both stdout and stderr to /dev/null
+alias -g NUL='&>/dev/null'
+
+# Access with ~name syntax, e.g., cd ~dot or ls ~dot
+hash -d dot="$HOME/dotfiles/"
