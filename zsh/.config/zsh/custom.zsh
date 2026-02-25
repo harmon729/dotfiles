fb119260847192656e86a891126ac1222e832b25
Harmon Hsu <xhx1719@gmail.com>
2026-02-25 14:41:19 +0800

    feat(zsh): ✨ enable zmv for advanced batch renaming and moving

--- zsh/.config/zsh/custom.zsh
+++ zsh/.config/zsh/custom.zsh
@@ -12,3 +12,15 @@ function auto_la() {
 }
 autoload -Uz add-zsh-hook
 add-zsh-hook chpwd auto_la
+
+# zmv
+# Usage examples:
+# zmv '(*).log' '$1.txt'           # Rename .log to .txt
+# zmv -w '*.log' '*.txt'           # Same thing, simpler syntax
+# zmv -n '(*).log' '$1.txt'        # Dry run (preview changes)
+# zmv -i '(*).log' '$1.txt'        # Interactive mode (confirm each)
+# Enable zmv
+autoload -Uz zmv
+# Helpful aliases for zmv
+alias zcp='zmv -C'  # Copy with patterns
+alias zln='zmv -L'  # Link with patterns
