0d34348ebf614cde5c9fee800391fddc43c7e379
Harmon Hsu <xhx1719@gmail.com>
2026-04-28 10:46:47 +0800

    perf(install_brew): ⚡ add authorize execute for brew_root

--- scripts/install_brew.sh
+++ scripts/install_brew.sh
@@ -3,6 +3,11 @@
 sudo apt-get update
 sudo apt-get install -y -- build-essential procps curl file git
 
+BREW_ROOT="/home/linuxbrew/.linuxbrew"
+if [[ -d "${BREW_ROOT}" ]]; then
+  sudo chown -R "$(whoami)" "${BREW_ROOT}"
+fi
+
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 if ! command -v brew; then
